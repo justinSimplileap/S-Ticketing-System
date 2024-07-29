0ac98f798980cb251c2494ee2ef7c387745628ed
Deepak <deepakpathak231201@gmail.com>
2024-07-29 13:49:48 +0530

    comment: if the user is not logged in then show only the login page

--- src/Components/common/ClientComponent.tsx
+++ src/Components/common/ClientComponent.tsx
@@ -4,8 +4,11 @@ import Sidebar from "@/Components/common/Sidebar";
 import PassSidebar from "@/Components/common/PassSidebar";
 import SidebarSuperAdmin from "../common/SidebarSuperAdmin";
 import TopBar from "./TopBar";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
+import Loader from "./Loader";
+import toast from "react-hot-toast";
+import { Toaster } from "react-hot-toast";
 
 export default function ClientComponent({
   children,
@@ -13,13 +16,25 @@ export default function ClientComponent({
   children: React.ReactNode;
 }) {
   const pathname = usePathname();
+  const router = useRouter();
   const [shouldHideSidebar, setShouldHideSidebar] = useState(false);
   const [shouldShowPassSidebar, setShouldShowPassSidebar] = useState(false);
   const [superSidebar, setSuperSidebar] = useState(false);
   const [shouldHideTopBar, setShouldHideTopBar] = useState(false);
   const [isSidebarExpanded, setIsSidebarExpanded] = useState(false); // Add state for sidebar expansion
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
+    const token = localStorage.getItem('token');
+    const publicPaths = ["/login"]
+    const currentPath = pathname.split('?')[0];
+
+    if (!token && !publicPaths.includes(currentPath)) {
+      toast.error("You must be logged in to view this page.");
+      router.push('/login');
+      return;
+    }
+
     const noSidebarRoutes = [
       "/login",
       "/reset",
@@ -50,6 +65,7 @@ export default function ClientComponent({
     setShouldShowPassSidebar(passSidebarRoutes.includes(pathname));
     setSuperSidebar(superSidebarRoutes.includes(pathname));
     setShouldHideTopBar(noTopBarRoutes.includes(pathname));
+    setIsLoading(false);
   }, [pathname]);
 
   // return (
@@ -102,8 +118,13 @@ export default function ClientComponent({
    
   const pathName = usePathname();
 
+  if (isLoading) {
+    return <Loader />
+  }
+
   return (
     <div className="flex">
+      <Toaster />
       {!sidebarRoutes.includes(pathName) && <Sidebar isExpanded={isSidebarExpanded} setIsSidebarExpanded={setIsSidebarExpanded}/>}
       {passwordSidebarRoutes.includes(pathname) && <PassSidebar />}
       {superAdminSidebarRoutes.includes(pathname) && <SidebarSuperAdmin isExpanded={isSidebarExpanded} setIsSidebarExpanded={setIsSidebarExpanded}/>}
